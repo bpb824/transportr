3a912f598ee4b34c0b19430469dc25137d7e11fc
bpb824 <bryanpblanc@gmail.com>
2015-12-21 20:54:07 -0800

    nelson-nygaard stuff

--- inst/staticdocs/index.R
+++ inst/staticdocs/index.R
@@ -3,3 +3,13 @@ sd_section(
   "These functions are used to interact with Google APIs.",
   c("decodeLineR", "getGoogleDirections","lines2points","route2shape")
 )
+sd_section(
+  "GTFS Utilities",
+  "These functions are used to download and prepare General Transit Feed Specification (GTFS) data.",
+  c("fetchFeed","spatialTransitShapes")
+)
+sd_section(
+  "General Use Functions",
+  "These functions are used to peform various intermediate data handling.",
+  c("list2frame")
+)
