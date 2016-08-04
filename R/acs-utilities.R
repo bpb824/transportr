60295735bdf631029fbbbbe29d64735152ec6f2d
Bryan Blanc <bryanpblanc@gmail.com>
2016-08-04 16:18:16 -0700

    revised acs query tables

--- R/acs-utilities.R
+++ R/acs-utilities.R
@@ -26,7 +26,7 @@ transit_pops = function(geog,endyear=2014, span=5){
   results$total_pop = data$`Total Population: Total`
   print(paste0(round(1/8*100,0),"% done with queries"))
 
-  #Poverty
+  #Low Income
   tid = "C17002"
   result = acs.fetch(endyear = endyear,span=span,geography=geog,table.number =tid,col.names = "pretty")
   data = as_data_frame(result@estimate)
@@ -38,7 +38,8 @@ transit_pops = function(geog,endyear=2014, span=5){
              `Ratio of Income to Poverty Level in the Past 12 Months: 1.25 to 1.49`) %>%
     dplyr::select(total,below_150) %>% dplyr::mutate(below_150_prop= below_150/total)
   
-  results$poverty = props$below_150_prop 
+  results$low_income_prop = props$below_150_prop 
+  results$low_income_n = props$below_150
   print(paste0(round(2/8*100,0),"% done with queries"))
 
   #Older Adults
@@ -56,8 +57,8 @@ transit_pops = function(geog,endyear=2014, span=5){
     dplyr::ungroup() %>%
     dplyr::mutate(old_prop = old/total)
 
-  results = results %>% left_join(props %>% dplyr::select(geography,old_prop) %>% 
-                                    dplyr::rename(older_adults=old_prop,NAME=geography),by="NAME")
+  results = results %>% left_join(props %>% dplyr::select(geography,old_prop,old) %>% 
+                                    dplyr::rename(older_adults_prop=old_prop,older_adults_n=old,NAME=geography),by="NAME")
   print(paste0(round(3/8*100,0),"% done with queries"))
 
   #Youth 10-17
@@ -73,8 +74,8 @@ transit_pops = function(geog,endyear=2014, span=5){
     dplyr::group_by(geography,total) %>% dplyr::summarise(young = sum(value))  %>%
     dplyr::ungroup() %>% dplyr::mutate(young_prop = young/total)
 
-  results = results %>% left_join(props %>% dplyr::select(geography,young_prop) %>% 
-                                    dplyr::rename(youth=young_prop,NAME=geography),by="NAME")
+  results = results %>% left_join(props %>% dplyr::select(geography,young_prop,young) %>% 
+                                    dplyr::rename(youth_prop=young_prop,youth_n=young,NAME=geography),by="NAME")
   print(paste0(round(4/8*100,0),"% done with queries"))
 
   #College Age 18-24
@@ -91,42 +92,42 @@ transit_pops = function(geog,endyear=2014, span=5){
     group_by(geography,total) %>% dplyr::summarise(young = sum(value)) %>% 
     dplyr::ungroup() %>%dplyr::mutate(young_prop = young/total)
 
-  results = results %>% left_join(props %>% dplyr::select(geography,young_prop) %>% 
-                                    dplyr::rename(college_age=young_prop,NAME=geography),by="NAME")
+  results = results %>% left_join(props %>% dplyr::select(geography,young_prop,young) %>% 
+                                    dplyr::rename(college_age_prop=young_prop,college_age_n=young,NAME=geography),by="NAME")
   print(paste0(round(5/8*100,0),"% done with queries"))
 
   #Persons with disabilities
-  tid = "B18101"
+  tid = "C21007"
   result = acs.fetch(endyear = endyear,span=span,geography=geog,table.number =tid,col.names = "pretty")
   data = as_data_frame(result@estimate)
   sub = data %>% dplyr::select(contains("Total"), contains("With a disability"))
   sub$geography = names
-  props= sub %>% dplyr::rename(total=`Sex by Age by Disability Status: Total:`) %>%
+  props= sub %>% dplyr::rename(total=`Age by Veteran Status by Poverty Status in the Past 12 Months by Disability Status for the Civilian Population 18+ Years: Total:`) %>%
     tidyr::gather(key,value,-geography,-total) %>% 
     dplyr::distinct() %>%
     dplyr::select(-key) %>%
     group_by(geography,total) %>% dplyr::summarise(disabled = sum(value)) %>% 
     dplyr::ungroup() %>% dplyr::mutate(disabled_prop = disabled/total)
 
-  results = results %>% left_join(props %>% dplyr::select(geography,disabled_prop) %>% 
-                                    dplyr::rename(disabled=disabled_prop,NAME=geography),by="NAME")
+  results = results %>% left_join(props %>% dplyr::select(geography,disabled_prop,disabled) %>% 
+                                    dplyr::rename(pwd_18up_prop=disabled_prop,pwd_18up_n=disabled,NAME=geography),by="NAME")
   print(paste0(round(6/8*100,0),"% done with queries"))
 
   #Households without Vehicles
-  tid = "B08201"
+  tid = "B25044"
   result = acs.fetch(endyear = endyear,span=span,geography=geog,table.number =tid,col.names = "pretty")
   data = as_data_frame(result@estimate)
   sub = data %>% dplyr::select(contains("Total"), contains("No vehicle available"))
   sub$geography = names
-  props= sub %>% dplyr::rename(total=`HOUSEHOLD SIZE BY VEHICLES AVAILABLE: Total:`) %>%
+  props= sub %>% dplyr::rename(total=`Tenure by Vehicles Available: Total:`) %>%
     tidyr::gather(key,value,-geography,-total) %>% 
     dplyr::distinct() %>%
     dplyr::select(-key) %>%
     group_by(geography,total) %>% dplyr::summarise(zero_vehicles = sum(value)) %>%
     dplyr::ungroup() %>% dplyr::mutate(zero_vehicles_prop = zero_vehicles/total)
 
-  results = results %>% left_join(props %>% dplyr::select(geography,zero_vehicles_prop) %>% 
-                                    dplyr::rename(no_vehicles=zero_vehicles_prop,NAME=geography),by="NAME")
+  results = results %>% left_join(props %>% dplyr::select(geography,zero_vehicles_prop,zero_vehicles) %>% 
+                                    dplyr::rename(no_vehicles_hh_prop=zero_vehicles_prop, no_vehicles_hh_n=zero_vehicles,NAME=geography),by="NAME")
   print(paste0(round(7/8*100,0),"% done with queries"))
 
   #Limited English
@@ -142,8 +143,8 @@ transit_pops = function(geog,endyear=2014, span=5){
     group_by(geography,total) %>% dplyr::summarise(no_english = sum(value)) %>% 
     dplyr::ungroup() %>% dplyr::mutate(no_english_prop = no_english/total)
 
-  results = results %>% left_join(props %>% dplyr::select(geography,no_english_prop) %>% 
-                                    dplyr::rename(non_english=no_english_prop,NAME=geography),by="NAME")
+  results = results %>% left_join(props %>% dplyr::select(geography,no_english_prop,no_english) %>% 
+                                    dplyr::rename(lep_prop=no_english_prop,lep_n=no_english,NAME=geography),by="NAME")
   print(paste0(round(8/8*100,0),"% done with queries"))
 
   return(results)
